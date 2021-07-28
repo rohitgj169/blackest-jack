f90b9024d84a69845778fe5ea452e2ba6fb2f81e
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-07-28 13:36:58 -0400

    draw deal shuffle construct deck

--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import './Deck.css'
+import React from "react";
 
-export default function Deck({deckState, setDeckState}) {
-    return (
-        <div className="Deck">
-            Deck Box
-            {/* <p>{deckState[51].value} of {deckState[51].suit}</p> */}
-        </div>
-    )
+import "./Deck.css";
+
+export default function Deck({ deckState, setDeckState }) {
+  return (
+    <div className="Deck">
+      Deck Box
+      {/* <p>{deckState[51].value} of {deckState[51].suit}</p> */}
+    </div>
+  );
 }
--- src/utilities/cards.js
+++ src/utilities/cards.js
@@ -1,9 +1,11 @@
+let shuffle = require('lodash.shuffle');
+
 function card(value,suit){
     this.value=value;
     this.suit=suit;
 }
 
-export function deck(){
+function deck(){
     this.values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace'];
     this.suits = ['Clubs','Diamonds','Hearts','Spades'];
     let cards = [];
@@ -17,12 +19,44 @@ export function deck(){
     return cards;
 }
 
-export function shuffle(deck){
-    return null;
+function shuffleDeck(deck){
+    let shuffledDeck = shuffle(deck);
+    // console.log(shuffledDeck);
+    return shuffledDeck;
     // randomly change indexes of cards in deck array
 }
 
-export function draw(deck){
-    return null;
+function draw(deck){
     // draw a random card from the deck
+    // returns card object
+    let card = deck.pop();
+    console.log(card);
+    return card;
+}
+
+function deal(deck){
+    let playerDeal = [];
+    let computerDeal = [];
+
+    playerDeal.push(draw(deck));
+    computerDeal.push(draw(deck));
+    playerDeal.push(draw(deck));
+    computerDeal.push(draw(deck));
+
+    return [playerDeal, computerDeal];
+
+}
+
+function bustCheck(hand){
+    let total = 0;
+    hand.forEach((card) => {
+      
+    })
+}
+
+module.exports = {
+  deck,
+  shuffle:shuffleDeck,
+  draw,
+  deal,
 }
