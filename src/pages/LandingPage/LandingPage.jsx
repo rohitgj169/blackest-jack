5c52d049ce533a971da2f480401747f088d18cf4
mattius9 <matthew.krasucki@gmail.com>
2021-08-03 10:57:32 -0400

    landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
   return (
     <div className="landing-page-container">
       <Link className="battle-link" to="/battle">The Blackest Jack</Link>
-      <div className="description">Welcome to Blackest Jack, a simple blackjack game created by <a href="https://github.com/mattius9">Matthew Krasucki</a> and <a href="http://rohitjacob.tech/">Rohit Jacob</a></div>
+      <div className="description">Welcome to Blackest Jack, a blackjack game created by <a href="https://github.com/mattius9">Matthew Krasucki</a> and <a href="http://rohitjacob.tech/">Rohit Jacob</a></div>
     </div>
   )
 }
